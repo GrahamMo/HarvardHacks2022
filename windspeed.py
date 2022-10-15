8ecdfb99851f1e6cf0c003208cb573fe303486c3
kylrw <kwitvoet03@gmail.com>
2022-10-15 19:43:12 -0400

    Calculation for wind power output

--- windspeed.py
+++ windspeed.py
@@ -1,18 +1,34 @@
 import requests, json, os
 import meteomatics.api as api
+import math
 
 import LoginCredentials
 
 def get_wind_data():
     api_key = os.getenv('api_key')
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    lat = 35
-    lon = 139
+    lat = 42.380960
+    lon = -71.125240
     complete_url = base_url + "appid=" + api_key + f"&lat={lat}&lon={lon}"
     response = requests.get(complete_url)
     x = response.json()
     print(x['wind']['speed'])
 
-    return True
+    return (x['wind']['speed'])
 
-get_wind_data()
+def get_wind_power(radius,velocity):
+
+    rho = 1.225 #average wind density is 1.225 kg/m^3
+
+    area = math.pi * (radius ** 2)
+
+    power = 0.5 * rho * area * (velocity ** 3)
+
+    power = round(power,2)
+
+    return power
+
+wind_speed = get_wind_data()
+expected_power_output = get_wind_power(2,wind_speed)
+
+print("Expected power output is: " + str(expected_power_output ) + " watts")
