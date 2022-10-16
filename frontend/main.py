38c805783537c5541ec36646e56d004aa8050bf5
michael-fedotov <m_fedotov@hotmail.com>
2022-10-15 20:19:29 -0400

    Solar Page
    Added class funcitonaly

--- frontend/main.py
+++ frontend/main.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  
-    return Flask.render_template('newuser.html', name="Please enter your info")
+    return flask.render_template('index.html', name="Please enter your info")
 
 
 if __name__ == "__main__":
+    # app = create_app()
     app.run(debug=True)
--- solar.py
+++ solar.py
@@ -1,5 +1,6 @@
 # import requests
 # import selenium
+from inspect import CORO_CLOSED
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,83 +11,84 @@ from geopy.geocoders import Nominatim
 # from googlemaps import GoogleMaps
 
 
-def coordinates(address):
-    geolocator = Nominatim(user_agent="Solar Calc")
-    location = geolocator.geocode(address)
-    return location.latitude, location.longitude
+class SolarPanel():
+    def __init__(self, address):
+        self.address = address
+        self.latitude, self.longitude = self.coordinates()
+        self.data = self.get_data()
+
+    def coordinates(self):
+        geolocator = Nominatim(user_agent="Solar Calc")
+        location = geolocator.geocode(self.address)
+        return location.latitude, location.longitude
 
 
-def get_full_url(address1):
-    latitude, longitude = coordinates(address1)
-    base_url = "https://sunroof.withgoogle.com/building/"
-    return base_url + str(latitude) + '/' + str(longitude) + '/' + '#?f=buy'
+    def get_full_url(self):
+        latitude, longitude = self.coordinates()
+        base_url = "https://sunroof.withgoogle.com/building/"
+        return base_url + str(latitude) + '/' + str(longitude) + '/' + '#?f=buy'
 
 
-def get_data():
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless')
-    # driver = webdriver.Chrome(options=op)
-    driver = webdriver.Chrome('/Users/michaelfedotov/Documents/HackHarvard/chromedriver', options=op)
-    driver.get(get_full_url("23 Harvard St Somerville, MA 02143"))
+    def get_data(self):
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        # driver = webdriver.Chrome(options=op)
+        driver = webdriver.Chrome('/Users/michaelfedotov/Documents/HackHarvard/chromedriver', options=op)
+        driver.get(self.get_full_url())
 
-    html = driver.page_source
+        html = driver.page_source
 
-    main_data = {
-        'num_hours_solar': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[1]/ul/li[1]/div[2]").text,
-        'sqft_avail': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[1]/ul/li[2]/div[2]").text,
-        'savings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[2]/div[1]/div[1]").text
-    }
+        main_data = {
+            'num_hours_solar': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[1]/ul/li[1]/div[2]").text,
+            'sqft_avail': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[1]/ul/li[2]/div[2]").text,
+            'savings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[1]/div[2]/md-card[2]/div[1]/div[1]").text
+        }
+            
+
+        envirionmental_impact = {
+            'carbon-dioxide': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[1]/div[2]").text,
+            'passenger-cars': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[2]/div[2]").text,
+            'tree-seedlings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[3]/div[2]").text
+        }
+
+
+        cost = {
+            'upfront-with-incentives': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[1]/div[1]").text,
+            '20-year-benefits-': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[2]/div[1]").text,
+            # '20-year-savings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[3]/div[1]").text,
+            'years-til-payback': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[4]/div[1]").text,
+            'up-front-cost-of-installation': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/show-more/div/table/tbody/tr[1]/td[2]/span").text,
+        }
+        # envirionmental_impact = driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div")
+        # # print("env", envirionmental_impact.text)
+        # for impact in envirionmental_impact.find_elements(By.CLASS_NAME, "place-metrics-cell has-icon"):
+        #     print(impact.text)
+        #     print(1)
+
+        # print(envirionmental_impact)
+
+        # bs = BeautifulSoup(html)
+        # print(bs)
+        # bsobject = bs.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div")
+        # for tag in bsobject.find_all("div", {"class": "place-metrics-cell has-icon"}):
+        #     print(tag.text)
+
+        driver.close()
         
+        data_dict = {
+            "main_date": main_data,
+            "envirionmental_impact": envirionmental_impact,
+            "cost": cost
+        }
 
-    envirionmental_impact = {
-        'carbon-dioxide': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[1]/div[2]").text,
-        'passenger-cars': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[2]/div[2]").text,
-        'tree-seedlings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div/place-metrics-cell[3]/div[2]").text
-    }
-
-
-    cost = {
-        'upfront-with-incentives': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[1]/div[1]").text,
-        '20-year-benefits-': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[2]/div[1]").text,
-        # '20-year-savings': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[3]/div[1]").text,
-        'years-til-payback': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/div/cost-cell[4]/div[1]").text,
-        'up-front-cost-of-installation': driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[3]/md-card/md-card-content/show-more/div/table/tbody/tr[1]/td[2]/span").text,
-    }
-    # envirionmental_impact = driver.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div")
-    # # print("env", envirionmental_impact.text)
-    # for impact in envirionmental_impact.find_elements(By.CLASS_NAME, "place-metrics-cell has-icon"):
-    #     print(impact.text)
-    #     print(1)
-
-    # print(envirionmental_impact)
-
-    # bs = BeautifulSoup(html)
-    # print(bs)
-    # bsobject = bs.find_element(By.XPATH, "/html/body/div[1]/address-view/div[1]/div/div/section[2]/div/md-content[2]/md-card/md-card-content/div")
-    # for tag in bsobject.find_all("div", {"class": "place-metrics-cell has-icon"}):
-    #     print(tag.text)
-
-    driver.close()
-    
-    data_dict = {
-        "main_date": main_data,
-        "envirionmental_impact": envirionmental_impact,
-        "cost": cost
-    }
-
-    print(data_dict)
-    return data_dict
-
-def send_data(): 
-    elemNumHours, sqft_avail, savings = get_data()
-
-    json_out = {'elemNumHours': elemNumHours, 
-        'sqft_avail': sqft_avail, 
-        'savings': savings}
-
-    return json_out # todo ad 
+        return data_dict
 
     # function for monthly electricity
+
+
+newdata = SolarPanel("23 Harvard St Somerville, MA 02143")
+
+
 
 
 
