744d42d1816530e662f667c04d5f05d5c73eb275
michael-fedotov <m_fedotov@hotmail.com>
2022-10-16 10:12:16 -0400

    side branch

--- Front-End/main.py
+++ Front-End/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  
-    return flask.render_template('index.html', name="Please enter your info")
+    return "Hellow  World!"
 
 
 if __name__ == "__main__":
